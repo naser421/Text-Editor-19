a77776aa3bf6204a1c80b2811974ee0d812c6c26
Naser Suleiman <nasersuleiman421@gmail.com>
2023-03-08 18:32:18 -0800

    subCache route

--- client/source.js
+++ client/source.js
@@ -4,3 +4,18 @@ const { registerRoute } = require('workbox-routing');
 const { CresponsePlugin } = require('workbox-cacheable-response');
 const { PluginExpire } = require('workbox-expiration');
 const { SubcacheRoute } = require('workbox-precaching/SubcacheRoute');
+
+
+SubcacheRoute(self.__WB_MANIFEST);
+
+var pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CresponsePlugin({
+      statuses: [0, 200],
+    }),
+    new PluginExpire({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
